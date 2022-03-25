1193471c1ad1cc2d5e2f307c9b3581d4063a8b6c
ART <pakkawat.kh@gmail.com>
2022-03-26 00:45:28 +0700

    update

--- src/pages/user.vue
+++ src/pages/user.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>user</div>
+  <div class="mainPadding brx" style="width: 100%; margin: auto">
+    <div>
+      <div align="right" class="brx" style="width: 100%">
+        <q-btn label="เพิ่มผู้ใช้งาน" @click="addNewUser()" />
+      </div>
+      <!-- table header -->
+      <div class="q-pt-md">
+        <div class="colTable">ลำดับ</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.colTable {
+  width: 11.1%;
+}
+</style>
