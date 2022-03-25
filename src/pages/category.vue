8d95f83ec26c299791c09dc288fdbe477114a155
ART <pakkawat.kh@gmail.com>
2022-03-26 01:22:57 +0700

    data for category

--- src/pages/category.vue
+++ src/pages/category.vue
@@ -3,7 +3,17 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      category: [
+        { id: 1, category: "โรแมนติก" },
+        { id: 2, category: "แฟนตาซี" },
+        { id: 5, category: "แอคชั่น" },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
