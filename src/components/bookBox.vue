f8e05f23b4a57689ac791f252cac47a0a829d498
ohm <destiny300940@gmail.com>
2022-03-31 18:51:05 +0700

    update components cartoon box

---
+++ src/components/bookBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="cartoonBox">
+      <div class="q-pt-md" align="center">
+        <img width="85%" src="../../public/poster/bleach.png" alt="" />
+      </div>
+      <div align="left" class="q-pl-md">
+        <div class="font16">Bleach</div>
+        <div class="font16">เทพมรณะ</div>
+      </div>
+      <div class="row">
+        <div class="font12 q-pl-md">ตอนที่ 12</div>
+        <div class="col"></div>
+        <div class="font12 q-pr-md">ยอดเข้าชม 130</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.cartoonBox {
+  width: 100%;
+  aspect-ratio: 230 / 370;
+  background: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+</style>
